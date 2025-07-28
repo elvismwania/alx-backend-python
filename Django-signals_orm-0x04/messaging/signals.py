46fcb6e4f20b71258308c6cdfb994da7c947ef0f
Elvis mwania <91766281+elvismwania@users.noreply.github.com>
2025-07-28 12:30:51 +0300

    signals.py

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,13 +1,18 @@
-
+# Django-signals_orm-0x04/messaging/signals.py
 
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import Message, Notification
+from django.contrib.auth.models import User
 
 @receiver(post_save, sender=Message)
 def create_notification_on_message(sender, instance, created, **kwargs):
+    """
+    Signal handler to create a Notification when a new Message is saved.
+    """
     if created:
         Notification.objects.create(
             user=instance.receiver,
             message=instance
         )
+        print(f"Notification created for {instance.receiver.username} due to new message from {instance.sender.username}")
