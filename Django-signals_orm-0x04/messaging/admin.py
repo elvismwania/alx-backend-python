9a3ff4f0413016c2e800f12462f30a4b53d54ac1
Elvis mwania <91766281+elvismwania@users.noreply.github.com>
2025-07-28 12:37:46 +0300

    admin.py

--- Django-signals_orm-0x04/messaging/admin.py
+++ Django-signals_orm-0x04/messaging/admin.py
@@ -1,12 +1,12 @@
 # Django-signals_orm-0x04/messaging/admin.py
 
 from django.contrib import admin
-from .models import Message, Notification
+from .models import Message, Notification, MessageHistory
 
 @admin.register(Message)
 class MessageAdmin(admin.ModelAdmin):
-    list_display = ('sender', 'receiver', 'content', 'timestamp')
-    list_filter = ('timestamp', 'sender', 'receiver')
+    list_display = ('sender', 'receiver', 'content', 'timestamp', 'edited') # Added 'edited'
+    list_filter = ('timestamp', 'sender', 'receiver', 'edited') # Added 'edited'
     search_fields = ('content', 'sender__username', 'receiver__username')
     date_hierarchy = 'timestamp'
     raw_id_fields = ('sender', 'receiver') # For easier selection if many users
@@ -18,3 +18,7 @@ class NotificationAdmin(admin.ModelAdmin):
     search_fields = ('user__username', 'message__content')
     date_hierarchy = 'timestamp'
     raw_id_fields = ('user', 'message') # For easier selection
+
+@admin.register(MessageHistory)
+class MessageHistoryAdmin(admin.ModelAdmin):
+    list_display = ('message', 'old_conten
