167db3175e7f0d16f58b247d9a1d3723b23a985b
Elvis mwania <91766281+elvismwania@users.noreply.github.com>
2025-07-28 12:31:25 +0300

    apps.py

--- Django-signals_orm-0x04/messaging/apps.py
+++ Django-signals_orm-0x04/messaging/apps.py
@@ -1,9 +1,12 @@
-from django.apps import AppConfig
+# Django-signals_orm-0x04/messaging/apps.py
 
+from django.apps import AppConfig
 
 class MessagingConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'messaging'
-    
+    verbose_name = "Messaging System"
+
     def ready(self):
+        # Import signals here to ensure they are connected when the app is ready
         import messaging.signals
